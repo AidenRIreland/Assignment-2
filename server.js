91205a2c0753f8bfc2a1b35f99f9c48164427e41
AidenRIreland <aidenireland2004@gmail.com>
2023-10-15 16:24:27 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+//const { error } = require('console');
+
+const app = express();
+const MONGODB_URL = 'mongodb://localhost:27017/Marketplace'; //Connect to the db
+const PORT = 3000;
+app.use(express.json()); // This allows you to use JSON data in POST requests
+app.use(cors());
+mongoose.connect(MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>{console.log('Connected to MongoDB')})
+.catch(error =>{console.error('Error connecting to MongoDB:',error.message)});
+app.get('/', (req, res) => {  
+    res.json({ "message": "Welcome to Marketplace application." });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
